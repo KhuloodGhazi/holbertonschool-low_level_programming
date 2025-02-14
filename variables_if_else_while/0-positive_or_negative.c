24df7a13256c40d47de13cec6682bbfd0e85d3fc
KhuloodGhazi <khulood.gmo@gmail.com>
2025-02-14 11:57:32 +0000

    fixing

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
+	/* Initialize random number generator */
 	srand(time(0));
+	/* Generate random number */
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
