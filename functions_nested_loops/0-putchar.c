d8f2a5547aad3b56022f884589639c7c95602ecf
KhuloodGhazi <khulood.gmo@gmail.com>
2025-02-16 16:00:33 +0000

    fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,20 +3,22 @@
 /**
  * main - Entry point
  *
+ * Description: Prints "_putchar" followed by a new line
+ * using the _putchar function.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char *str = "_putchar";
-    int i = 0;
+	char *str = "_putchar";
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 
-    return (0);
+	return (0);
 }
-
