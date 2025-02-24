49d6c170109c151eca1e54de700aab1371359f40
KhuloodGhazi <khulood.gmo@gmail.com>
2025-02-24 14:36:43 +0000

    fix

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
-x#include "main.h"
+#include "main.h"
 
 /**
- * _isupper - Checks for uppercase character.
- * @c: The character to check.
- *
- * Return: 1 if c is uppercase, 0 otherwise.
+ * _isupper - checks for uppercase character.
+ * @c: the character to check
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	return (0);
+    if (c >= 'A' && c <= 'Z')
+        return 1;
+    else
+        return 0;
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,8 +2,5 @@
 #define MAIN_H
 
 int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-int _putchar(char c);
 
 #endif /* MAIN_H */
