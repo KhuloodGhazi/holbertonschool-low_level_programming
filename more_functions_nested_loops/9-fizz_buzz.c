fb10a05ce494fe16c3650e1911cb036379fd0e7b
KhuloodGhazi <khulood.gmo@gmail.com>
2025-02-25 16:38:01 +0000

    fixing

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers from 1 to 100, with Fizz, Buzz, and FizzBuzz replacements.
+ * main - Prints numbers from 1 to 100 with Fizz, Buzz, and FizzBuzz.
+ *
+ * Description: For multiples of 3, print "Fizz" instead of the number.
+ * For multiples of 5, print "Buzz". For multiples of both, print "FizzBuzz".
+ * Each number or word is separated by a space.
  *
  * Return: Always 0.
  */
@@ -11,7 +15,8 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if ((i % 3 == 0)
+		    && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
