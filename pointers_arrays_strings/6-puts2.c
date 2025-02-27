6fa94e45a6b0356b8788b165caffc0ddc67cf9a1
KhuloodGhazi <khulood.gmo@gmail.com>
2025-02-27 14:19:36 +0000

    fix

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,21 @@
 #include "main.h"
+#include <stddef.h>  /* To define NULL */
 
 /**
- * puts2 - Prints every other character of a string, starting with the first.
+ * puts2 - Prints every other character of a string.
  * @str: Pointer to the string.
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	if (str == NULL) /* Edge case: NULL input */
+		return;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
+		if (i % 2 == 0) /* Print only even indices */
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
