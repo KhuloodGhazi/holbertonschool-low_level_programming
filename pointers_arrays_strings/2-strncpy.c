f1ae5e5bc02d738a02a0a45811998c7cd5e3a2fb
KhuloodGhazi <khulood.gmo@gmail.com>
2025-02-28 11:22:15 +0000

    fixing

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,28 @@
 #include "main.h"
 
 /**
- * _strncpy - Copies a string
- * @dest: Destination buffer
- * @src: Source string to copy
- * @n: Maximum number of bytes to copy
+ * _strncpy - Copies a string, up to n bytes.
+ * @dest: Destination buffer.
+ * @src: Source string.
+ * @n: Number of bytes to copy.
  *
- * Return: Pointer to the resulting string `dest`
+ * Return: Pointer to destination buffer.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    /* Copy at most `n` characters from `src` to `dest` */
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
 
-    /* If `src` is shorter than `n`, pad the rest with `\0` */
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
+	/* Fill the remaining space in dest with null bytes if src is shorter */
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
-    return (dest);
+	return (dest);
 }
