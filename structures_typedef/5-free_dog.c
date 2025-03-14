a688317ede290c4a2ed57e6fc401c1e55d553b6d
KhuloodGhazi <khulood.gmo@gmail.com>
2025-03-14 14:09:11 +0000

    fix

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,15 +2,15 @@
 #include "dog.h"
 
 /**
- * free_dog - Frees a dog structure
- * @d: The dog to free
+ * free_dog - Frees a dog
+ * @d: Pointer to the dog to be freed
  */
 void free_dog(dog_t *d)
 {
     if (d != NULL)
     {
-        free(d->name);  // Free the dynamically allocated memory for name
-        free(d->owner); // Free the dynamically allocated memory for owner
-        free(d);         // Finally, free the dog struct itself
+        free(d->name);  /* Free the dynamically allocated memory for name */
+        free(d->owner); /* Free the dynamically allocated memory for owner */
+        free(d);        /* Free the memory for the dog structure itself */
     }
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,9 +9,9 @@
  */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
 /* Define dog_t as an alias for struct dog */
@@ -21,5 +21,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);  /* Added function prototype for free_dog */
 
 #endif /* DOG_H */
