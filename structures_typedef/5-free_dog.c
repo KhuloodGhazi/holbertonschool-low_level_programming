56abb796326c18f20fe9bf31a810c699a43922bd
KhuloodGhazi <khulood.gmo@gmail.com>
2025-03-14 16:26:40 +0000

    fixing

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,15 +2,15 @@
 #include "dog.h"
 
 /**
- * free_dog - Frees a dog
- * @d: Pointer to the dog to be freed
+ * free_dog - Frees a dog structure.
+ * @d: Pointer to the dog structure to be freed.
  */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d->name);  /* Free the dynamically allocated memory for name */
-        free(d->owner); /* Free the dynamically allocated memory for owner */
-        free(d);        /* Free the memory for the dog structure itself */
-    }
+	if (d != NULL)
+	{
+		free(d->name);  /* Free allocated memory for name */
+		free(d->owner); /* Free allocated memory for owner */
+		free(d);        /* Free the memory allocated for struct itself */
+	}
 }
