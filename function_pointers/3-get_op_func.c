f347b362207f6e743c8cbb57b1bc7a48dac98284
KhuloodGhazi <khulood.gmo@gmail.com>
2025-03-16 18:55:29 +0000

    fix 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,11 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
-#include <stddef.h>  /* Add this to define NULL */
+#include <stddef.h"
 
 /**
  * get_op_func - Selects the correct function to perform the operation
- * @s: The operator passed as an argument to the program
+ * @s: The operator passed as argument
  *
- * Return: Pointer to the function corresponding to the operator
+ * Return: Pointer to the corresponding function, or NULL if invalid
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -15,16 +18,16 @@ int (*get_op_func(char *s))(int, int)
         {"*", op_mul},
         {"/", op_div},
         {"%", op_mod},
-        {NULL, NULL}  /* Ensure that NULL is declared correctly */
+        {NULL, NULL} /* End of operations array */
     };
     int i = 0;
 
     while (ops[i].op != NULL)
     {
-        if (*s == *(ops[i].op))  /* Compare operator */
-            return (ops[i].f);
+        if (strcmp(s, ops[i].op) == 0)
+            return (ops[i].f); /* Return function pointer */
         i++;
     }
 
-    return (NULL);  /* Return NULL if no matching operator is found */
+    return (NULL); /* Return NULL if no match is found */
 }
