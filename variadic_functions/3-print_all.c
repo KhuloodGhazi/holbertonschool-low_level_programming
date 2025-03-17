6d67a24dd6511cfd941a4c05396e35f017b15063
KhuloodGhazi <khulood.gmo@gmail.com>
2025-03-17 18:44:59 +0000

    add

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything based on the format string
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    char *separator = "";
+
+    va_start(args, format);
+
+    if (format) { /* First if: Checks if format is not NULL */
+        while (format[i]) {
+            if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') { /* Second if: checks for format specifiers */
+                if (format[i] == 'c')
+                    printf("%s%c", separator, va_arg(args, int));
+                else if (format[i] == 'i')
+                    printf("%s%d", separator, va_arg(args, int));
+                else if (format[i] == 'f')
+                    printf("%s%f", separator, va_arg(args, double)); /* 'f' promoted to double */
+                else if (format[i] == 's')
+                {
+                    str = va_arg(args, char *);
+                    if (!str)
+                        str = "(nil)";
+                    printf("%s%s", separator, str);
+                }
+                separator = ", ";
+            }
+            i++;
+        }
+    }
+
+    printf("\n");
+
+    va_end(args);
+}
