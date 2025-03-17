0af87f5a9341dbe7598b6a48f7e4105bdfd8954d
KhuloodGhazi <khulood.gmo@gmail.com>
2025-03-17 18:37:40 +0000

    fix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,42 +10,36 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *str;
-	char *separator = "";
+    va_list args;
+    unsigned int i = 0;
+    char *str;
+    char *separator = "";
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (format && format[i])
-	{
-		if (format[i] == 'c')
-		{
-			printf("%s%c", separator, va_arg(args, int));
-			separator = ", ";
-		}
-		else if (format[i] == 'i')
-		{
-			printf("%s%d", separator, va_arg(args, int));
-			separator = ", ";
-		}
-		else if (format[i] == 'f')
-		{
-			printf("%s%f", separator, va_arg(args, double)); /* 'f' promoted to double */
-			separator = ", ";
-		}
-		else if (format[i] == 's')
-		{
-			str = va_arg(args, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", separator, str);
-			separator = ", ";
-		}
-		i++;
-	}
+    while (format && format[i])
+    {
+        if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+        {
+            if (format[i] == 'c')
+                printf("%s%c", separator, va_arg(args, int));
+            else if (format[i] == 'i')
+                printf("%s%d", separator, va_arg(args, int));
+            else if (format[i] == 'f')
+                printf("%s%f", separator, va_arg(args, double)); /* 'f' promoted to double */
+            else if (format[i] == 's')
+            {
+                str = va_arg(args, char *);
+                if (!str)
+                    str = "(nil)";
+                printf("%s%s", separator, str);
+            }
+            separator = ", ";
+        }
+        i++;
+    }
 
-	printf("\n");
+    printf("\n");
 
-	va_end(args);
+    va_end(args);
 }
