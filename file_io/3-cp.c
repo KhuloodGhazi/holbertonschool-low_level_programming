b9c6e34f7a2059b915b19373be21752abfeaceb8
KhuloodGhazi <khulood.gmo@gmail.com>
2025-04-04 19:57:51 +0000

    fix 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,6 +18,44 @@ static void safe_close(int fd)
 }
 
 /**
+ * copy_loop - Reads from fd_from and writes to fd_to.
+ * @fd_from: File descriptor for the source file.
+ * @fd_to: File descriptor for the destination file.
+ * @file_from: Name of the source file.
+ * @file_to: Name of the destination file.
+ */
+static void copy_loop(int fd_from, int fd_to, const char *file_from,
+		      const char *file_to)
+{
+	ssize_t r, w;
+	char buffer[1024];
+
+	while (1)
+	{
+		r = read(fd_from, buffer, sizeof(buffer));
+		if (r == -1)
+		{
+			safe_close(fd_from);
+			safe_close(fd_to);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", file_from);
+			exit(98);
+		}
+		if (r == 0)
+			break;
+		w = write(fd_to, buffer, r);
+		if (w == -1 || (size_t)w != (size_t)r)
+		{
+			safe_close(fd_from);
+			safe_close(fd_to);
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+}
+
+/**
  * copy_file - Copies the content of file_from to file_to.
  * @file_from: Source file name.
  * @file_to: Destination file name.
@@ -27,10 +65,7 @@ static void safe_close(int fd)
 int copy_file(const char *file_from, const char *file_to)
 {
 	int fd_from, fd_to;
-	ssize_t r, w;
-	char buffer[1024];
 
-	/* Blank line after declarations */
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
@@ -41,27 +76,10 @@ int copy_file(const char *file_from, const char *file_to)
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close(fd_from);
+		safe_close(fd_from);
 		exit(99);
 	}
-	while ((r = read(fd_from, buffer, sizeof(buffer))) > 0)
-	{
-		w = write(fd_to, buffer, r);
-		if (w != r)
-		{
-			close(fd_from);
-			close(fd_to);
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
-	if (r == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+	copy_loop(fd_from, fd_to, file_from, file_to);
 	safe_close(fd_from);
 	safe_close(fd_to);
 	return (0);
@@ -84,4 +102,3 @@ int main(int argc, char *argv[])
 	copy_file(argv[1], argv[2]);
 	return (0);
 }
-
