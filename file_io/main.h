5d206358ea1c584b4fd2ece966f1e1bf5bbe9e2d
KhuloodGhazi <khulood.gmo@gmail.com>
2025-04-04 21:26:38 +0000

    fixing

--- file_io/main.h
+++ file_io/main.h
@@ -1,16 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
-/* POSIX standards */
-#include <unistd.h>
-
-/* OPEN, READ, WRITE, CLOSE */
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
